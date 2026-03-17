ed303a5347fca5900352004f1dde2971c68202ce
ryan r <2104523@cmontmorency.qc.ca>
2026-03-17 14:37:11 -0400

    test

--- conversion.php
+++ conversion.php
@@ -1,9 +1,30 @@
- <?php
- require "classThermistor.php";
-$Vadc = $_GET["Vadc"];
-$Vcc  = $_GET["Vcc"];
-$Rdiv = $_GET["Rdiv"];
-$t = new Thermistor();
-$t->setValues($Vadc,$Vcc,$Rdiv);
-echo "Temperature: ".$t->getTemp()." C";
+<?php
+ 
+if(isset($_GET['Vadc']) && isset($_GET['Vcc']) && isset($_GET['Rdiv']) && isset($_GET['unit']))
+{
+ 
+ $Vadc = $_GET['Vadc'];
+ $Vcc = $_GET['Vcc'];
+ $Rdiv = $_GET['Rdiv'];
+ $unit = $_GET['unit'];
+ 
+ require_once("classthermistor.php");
+ 
+ $aConverter = new Thermistor();
+ 
+    $aConverter->setVadc($Vadc);
+    $aConverter->setVcc($Vcc);
+    $aConverter->setRdiv($Rdiv);
+ 
+ $Temperature = $aConverter->calculerTemperature();
+ 
+ echo "temperature =". round($Temperature, 2) . "°";
+ 
+ }else{
+ 
+ echo "ERREUR: DES PARAMETRE SONT MANQUANT " ;
+ 
+ 
+ 
+ }
 ?>
